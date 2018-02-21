f28a084ccf3f6b7241e623460c2b13acc3bed332
Delphine <delphine.braive@gmail.com>
2018-02-21 10:02:28 +0100

    amb & cie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 import "../scss/app.scss";
-
-
-
+var Email = require('email').Email
+var myMsg = new Email(
+{ from: "me@example.com"
+, to:   "delphine.braive@gmail.com"
+, subject: "Knock knock..."
+, body: "Who's there?"
+})
+// if callback is provided, errors will be passed into it
+// else errors will be thrown
+myMsg.send(function(err){
+console.log(err)
+})
