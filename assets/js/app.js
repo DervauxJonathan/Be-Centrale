d516c0b9941ef0b7cd645c7ebf6679e462e00a1c
keloriane <kevin.flbt@gmail.com>
2018-04-05 11:46:51 +0200

    commit animation page cofunders join us ...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,46 @@
 import "../scss/app.scss";
-import axios from "axios"
+import "jquery";
+import { TimelineMax, TweenMax, Linear, Power1, TweenLite, Power4 } from 'gsap';
+require('animation.gsap');
+require('debug.addIndicators');
+const TimelineMax = require('TimelineMax');
+const ScrollMagic = require('ScrollMagic');
+import {ScrollScene} from 'scrollmagic';
+import axios from "axios";
+
+
+$(document).ready(function(){
+//animation on scroll//
+$('.section').each(function(){
+    var timeline = new TimelineMax();
+    var p = $(this).find('paragraphe');
+    var line = $(this).find('.line');
+    var button = $(this).find('.button');
+    var title = $(this).find('h2');
+    var bg = $(this).find('.bg');
+    var line=$(this).find('.line')
+      
+    timeline
+          .from(title, 0.25,{opacity:0, ease:Power0.easeInOut},"-=0.15")
+          .from(line, 0.25, {width:0 , ease:Power0.easeInOut})
+          .from(p, 0.25,{opacity:0, y:50,ease:Power1.easeOut})
+          .from(button,0.25,{opacity:0,x:-200})
+          .from(bg,0.5,{width:0});
+  
+    
+    var animaiton = new ScrollMagic.Scene({
+      triggerElement:this,
+      triggerHook:'0.75',
+      reverse:false
+         
+   }) 
+      .setTween(timeline) 
+      
+      .addIndicators()
+      .addTo(controller)
+        
+       
+      });
 
 function getDataFromRoom() {
     return axios.get('http://localhost:3000/room.json')
@@ -29,5 +70,41 @@ link.forEach((element) => {
                 roomImage.src = selectedRoom.image;
             }
         )
+        var rooms = new TimelineMax();
+        rooms
+          .fromTo($('.green_overlay'),0.5,{width:0},{width:"100%",ease: Power4.easeInOut})
+          .fromTo($('.green_overlay'),0.5,{x:"0%"},{x:"100%",ease: Power4.easeInOut})
+          .add('controll')
+          .from(roomImage,0.25,{x:"-100%",ease: Power4.easeInOut},"-=0.5")
+          .fromTo($('.grey_overlay'),0.5,{width:0,},{width:"100%",ease: Power4.easeInOut},"-=0.5")
+          .fromTo($('.grey_overlay'),0.5,{x:"0%"},{x:"100%",ease: Power4.easeInOut},"-=0.5")
+        //   .fromTo(roomName,0.5,{x:-150},{x:0,ease: Power4.easeInOut})
+          .from([roomCapacity,roomPeople,roomMaterial],0.5,{autoAlpha:0,y:20},'tween-=0.75')
+          //.set([roomCapacity,roomPeople,roomMaterial],{autoAlpha:1})
+          .from($('.paragraphe'),0.5,{width:0,ease:Power1.easeOut});
+          // TweenLite.from([roomCapacity,roomPeople,roomMaterial],0.7,{autoAlpha:0,y:20});
     })
 });
+$('#openMenu').click(function(){
+    $("#main").fadeIn(200);
+    $('#openMenu').animate({left:"-10%"});
+    $('#closeMenu').animate({left:"100%"});
+    var opening = new TimelineMax();
+    opening    
+      .from($('#logoSection'),.2,{x:"-50%"})
+      .from($('#menuSection'),.2,{x:"-70%"})
+      .from($('#service'),0.1,{x:-350})
+      .staggerFrom($('.menu'), 0.15, {x:-290}, 0.1)
+  });
+  
+  $('#closeMenu').click(function(){
+    $("#main").fadeOut(200);
+    $('#openMenu').animate({left:"2%"});
+    $('#closeMenu').animate({left:"110%"});
+    
+    //Rooms Transition //
+    
+  });
+
+
+ })
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 const imagePath = "./images";
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
@@ -7,15 +8,15 @@ const extractSass = new ExtractTextPlugin({
     disable: process.env.NODE_ENV === "development"
 });
 const browserSync = new BrowserSyncPlugin({
-    files: [
-        './assets/js/*',
-        './assets/scss/*',
-        './public/*.html',
-    ],
-    server: {
-        baseDir: 'public',
-        index: "public/index.html"
-    }
+        files: [
+            './assets/js/*',
+            './assets/scss/*',
+            './public/*.html',
+        ],
+        server: {
+            baseDir: 'public',
+            index: "public/index.html"
+        }
 });
 
 
@@ -29,18 +30,39 @@ module.exports = {
                 use: [{loader: "css-loader"}, {loader: "sass-loader"}], fallback: "style-loader"
             })
         },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {name: imagePath + '/[name].[ext]'}
-                }]
-            }
+        {
+            test: /\.js$/,
+            loader: "imports-loader?define=>false"
+        },    
+        {
+            test: /\.(png|jpg|jpeg|gif)$/,
+            use: [{
+            loader: 'file-loader',
+            options: {name: imagePath + '/[name].[ext]'}
+            }]
+        }
         ]
     },
+   
+   
+          
+    resolve: {
+        alias: {
+            "TweenLite": path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+            "TweenMax": path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+            "TimelineLite": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+            "TimelineMax": path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+            "ScrollMagic": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+            "animation.gsap": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+            "debug.addIndicators": path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+        }
+      },
+      
     plugins: [
-        extractSass, browserSync
-    ],
-    mode: "development"
+        new webpack.ProvidePlugin({
+            $:'jquery',
+            jQuery: 'jquery'
+        }),
+        extractSass,browserSync
+    ]
 };
-
